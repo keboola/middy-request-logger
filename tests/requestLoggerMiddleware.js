916dd5ed39429b196ee3c403130a5179cda87964
Miroslav Čillík <miro@keboola.com>
2019-07-24 14:20:31 +0200

    chore: fixed tests

--- tests/requestLoggerMiddleware.js
+++ tests/requestLoggerMiddleware.js
@@ -45,6 +45,27 @@ const handler = {
   },
 };
 
+const expectedOutput = {
+  resource: '/{proxy+}',
+  httpMethod: 'GET',
+  queryStringParameters: {},
+  pathParameters: {
+    proxy: 'users',
+  },
+  headers: {
+    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch, br',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Content-Type': 'application/json',
+    Host: 'xxxxxxxxxx.execute-api.us-east-1.amazonaws.com',
+  },
+  body: '{"name": "Sam"}',
+  sourceIp: '11.111.111.111',
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+  awsRequestId: null,
+  stage: 'prod',
+};
+
 describe('requestLoggerMiddleware', () => {
   beforeEach(() => {
     sinon.stub(console, 'log');
@@ -56,6 +77,6 @@ describe('requestLoggerMiddleware', () => {
     requestLogger.before(handler, () => {});
 
     expect(console.log.calledOnce, 'to be true');
-    expect(console.log.calledWith('{"resource":"/{proxy+}","httpMethod":"GET","queryStringParameters":{},"pathParameters":{"proxy":"users"},"headers":{"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8","Accept-Encoding":"gzip, deflate, sdch, br","Accept-Language":"en-US,en;q=0.8","Content-Type":"application/json","Host":"xxxxxxxxxx.execute-api.us-east-1.amazonaws.com"},"body":"{\\"name\\": \\"Sam\\"}","sourceIp":"11.111.111.111","userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36","awsRequestId":null}'), 'to be true');
+    expect(console.log.calledWith(JSON.stringify(expectedOutput)), 'to be true');
   });
 });
